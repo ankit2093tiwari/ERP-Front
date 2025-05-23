a83eea55156881ed2eba6dfce7baed6f8d3097e8
Muskan <muskan.nextupgrad@gmail.com>
2025-05-23 19:05:52 +0530

    changes

--- src/app/students/assign-roll-no/page.js
+++ src/app/students/assign-roll-no/page.js
@@ -294,7 +294,7 @@ const AssignRollNo = () => {
                 <Col>
                   {showButtons && !isEditing && (
                     <Button
-                      variant="warning"
+                      // variant="warning"
                       onClick={handleEditRollNo}
                       className="me-2"
                     >
