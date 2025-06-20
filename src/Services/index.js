55e92a791d89dbbcc6695cde1987de33f5e3f911
niteshNextupgrad <nitesh.nextupgrad@gmail.com>
2025-06-20 20:13:54 +0530

    modified:   src/Services/index.js

--- src/Services/index.js
+++ src/Services/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const BASE_URL = 'https://erp-backend-fys3n.onrender.com'
-export const BASE_URL = 'http://localhost:8000'
+export const BASE_URL = 'https://erp-backend-fys3n.onrender.com'
+// export const BASE_URL = 'http://localhost:8000'
 axios.interceptors.response.use(
     (response) => {
         if (response.data?.error === "Token expired") {
